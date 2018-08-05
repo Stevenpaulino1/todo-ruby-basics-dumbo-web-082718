6fa5ca59cbd9cda57f367d8fa504338990e55967
Steven Paulino <stevenpaulino1@hotmail.com>
2018-08-05 14:56:07 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value 
 end
 
-def argue
-  
+def argue(phrase)
+  return phrase
 end
 
-def greeting
-
+def greeting(greeting,name)
+  puts "#{greeting}, my name is #{name}"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  steven = "Shylene"
+  return "expert"
+  shylene = "steven"
 end
 
-def pizza_party
-
+def pizza_party(toping = "cheese")
+  puts toping
+  phrase = toping
 end
